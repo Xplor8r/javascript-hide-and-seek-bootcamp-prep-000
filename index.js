9cf66677f2360b5feee4055adb6d016b77ccfdcc
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-06 01:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+function getFirstSelector(selector) {
+  var sel = document.querySelector(selector);
+  return sel;
+}
+function nestedTarget () {
+  var targ = document.getElementsByTagName('div') [4];
+  return targ;
+}
+function increaseRankBy(n) {
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+function deepestChild() {
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
+  for (let i = 0; i < deep.length -1; i++) {
+    var chi = deep[i].querySelector('div');
+  }
+  return chi;
+}
