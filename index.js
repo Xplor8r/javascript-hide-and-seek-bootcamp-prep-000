1474e5b2498c6c18d7e94dad4ee5ce062fe8e4e5
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-06 00:30:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function getFirstSelector(selector) {
   return sel;
 }
 function nestedTarget () {
-  var tar = document.getElementsByTagName('div') [4];
-  return tar;
+  var targ = document.getElementsByTagName('div') [4];
+  return targ;
 }
 function increaseRankBy(n) {
-  var rank = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < rank.length; i++) {
     rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
   }
